57cf96f16571ee13128dbd28dd485d5ae366457d
Alejandro Lugo <alejandro.lugo.idoyaga@gmail.com>
2023-02-19 12:25:53 -0300

    Created controller for retrieving Real Estate records

--- force-app/main/default/lwc/propertyTileList/propertyTileList.js
+++ force-app/main/default/lwc/propertyTileList/propertyTileList.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire } from 'lwc';
+import getRealEstateProperties from '@salesforce/apex/RealEstatePropertyController.getRealEstateProperties'
 
-export default class PropertyTileList extends LightningElement {}
+export default class PropertyTileList extends LightningElement {
+
+    realEstateProperties;
+
+    @wire(getRealEstateProperties)
+    realEstateHandler({data,error}){
+        if(data){
+            console.log(data)
+            this.realEstateProperties = data
+        }
+        if(error){
+            console.error(error)
+        }
+    }
+}
