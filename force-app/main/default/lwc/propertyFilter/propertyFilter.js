fabcb11787600c5c77818beb1ffa04b2669a9133
Alejandro Lugo <alejandro.lugo.idoyaga@gmail.com>
2023-02-19 16:10:07 -0300

    Added filter capabilities for properties list

--- force-app/main/default/lwc/propertyFilter/propertyFilter.js
+++ force-app/main/default/lwc/propertyFilter/propertyFilter.js
@@ -6,12 +6,33 @@ import REAL_ESTATE_OBJECT from '@salesforce/schema/Real_Estate_Property__c';
 import TYPE_FIELD from '@salesforce/schema/Real_Estate_Property__c.Type__c';
 import SALE_TYPE_FIELD from '@salesforce/schema/Real_Estate_Property__c.Sale_Type__c';
 
+//Lightning Message Service and a message channel
+import { publish, MessageContext } from 'lightning/messageService';
+import PROPERTY_FILTERED_MESSAGE from '@salesforce/messageChannel/PropertyFiltered__c';
+
 // Constants
 const TYPE_ERROR = 'Error loading types'
 const SALE_TYPE_ERROR = 'Error loading sale types'
 
 export default class PropertyFilter extends LightningElement {
 
+    typeError = TYPE_ERROR
+    saleTypeError = SALE_TYPE_ERROR
+
+    filters = {
+        searchKey:'',
+        maxPrice:3000000,
+        saleType:'',
+        types: []
+    }
+
+    timer
+
+    /*Load context for LMS */
+    @wire(MessageContext)
+    messageContext;
+
+    /*Fetching picklist values for the filters */
     @wire(getObjectInfo, { objectApiName: REAL_ESTATE_OBJECT })
     realEstateObjectInfo;
 
@@ -27,37 +48,36 @@ export default class PropertyFilter extends LightningElement {
     })
     saleTypeOptions
 
-    typeError = TYPE_ERROR
-    saleTypeError = SALE_TYPE_ERROR
-
-    filters = {
-        searchKey:'',
-        maxPrice:3000000
-    }
-
-    typesSelected = [];
-    saleTypeSelected = '';
-
-    handleTypeChange(event) {
-        this.typesSelected = event.detail.value;
-        console.log(this.typesSelected)
-    }
-
-    handleSaleTypeChange(event) {
-        this.saleTypesSelected = event.detail.value;
-        console.log(this.saleTypesSelected)
-    }
-
+    /* Search Key handler */
     handleSearchKeyChange(event){
-        console.log(event.target.value)
-        this.filters.searchKey = {...this.filters, searchKey: event.target.value}
+        this.filters = {...this.filters, searchKey: event.target.value}
+        this.sendDataToPropertyList()
     }
 
+    /* Max price slider handler */
     handleMaxPriceChange(event){
-        console.log(event.target.value)
-        this.filters.searchKey = {...this.filters, maxPrice: event.target.value}
+        this.filters = {...this.filters, maxPrice: event.target.value}
+        this.sendDataToPropertyList()
     }
 
+    /* Type checkbox group handler */
+    handleTypeChange(event) {
+        this.filters = {...this.filters, types: event.target.value}
+        this.sendDataToPropertyList()
+    }
 
+    /* Sale type radio button handler */
+    handleSaleTypeChange(event) {
+        this.filters = {...this.filters, saleType: event.target.value}
+        this.sendDataToPropertyList()
+    }
+
+    sendDataToPropertyList(){
+        window.clearTimeout(this.timer)
+        this.timer = window.setTimeout(()=>{
+            publish(this.messageContext, PROPERTY_FILTERED_MESSAGE, { filters: this.filters });
+        }, 400)
+        
+    }
 
 }
--- force-app/main/default/lwc/propertyTileList/propertyTileList.js
+++ force-app/main/default/lwc/propertyTileList/propertyTileList.js
@@ -1,11 +1,59 @@
 import { LightningElement,wire } from 'lwc';
 import getRealEstateProperties from '@salesforce/apex/RealEstatePropertyController.getRealEstateProperties'
 
+//Lightning Message Service
+import {
+    subscribe,
+    unsubscribe,
+    APPLICATION_SCOPE,
+    MessageContext,
+} from 'lightning/messageService';
+import PROPERTY_FILTERED_MESSAGE from '@salesforce/messageChannel/PropertyFiltered__c';
+
 export default class PropertyTileList extends LightningElement {
+
+    subscription = null;
 
     realEstateProperties;
 
     filters = {}
+
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }
+
+    disconnectedCallback() {
+        this.unsubscribeToMessageChannel();
+    }
+
+    /* Load context for LMS */
+    @wire(MessageContext)
+    messageContext;
+
+    /* Subscribe to LMS channel */
+    subscribeToMessageChannel() {
+        if (!this.subscription) {
+            this.subscription = subscribe(
+                this.messageContext,
+                PROPERTY_FILTERED_MESSAGE,
+                (message) => this.handleMessage(message),
+                { scope: APPLICATION_SCOPE }
+            );
+        }
+    }
+
+    /* Unsubscribe to LMS channel */
+    unsubscribeToMessageChannel() {
+        unsubscribe(this.subscription);
+        this.subscription = null;
+    }
+
+    /* Handler for message received by propertyFilter */
+    handleMessage(message) {
+        console.log('received lms')
+        console.log(message)
+        this.filters = {...message.filters}
+    }
 
     @wire(getRealEstateProperties, {filters: '$filters'})
     realEstateHandler({data,error}){
